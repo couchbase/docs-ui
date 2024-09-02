e421846b1edf702460969427ebbf4a4c6f39dfcb
Hakim Cassimally <hakim.cassimally@couchbase.com>
2024-09-02 15:55:13 +0100

    couchbasecloud.atlassian.net

--- src/js/vendor/feedback.js
+++ src/js/vendor/feedback.js
@@ -6,7 +6,7 @@
     // var config = document.getElementById('feedback-script').dataset
     var script = document.createElement('script')
     // eslint-disable-next-line max-len
-    script.src = 'https://jira.issues.couchbase.com/s/d41d8cd98f00b204e9800998ecf8427e-T/-3ddrgv/b/7/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-GB&collectorId=69d1d56c' // prettier-ignore
+    script.src = 'https://couchbasecloud.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-3ddrgv/b/7/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-GB&collectorId=69d1d56c' // prettier-ignore
     document.body.appendChild(script)
   })
 })()
