9934c014d010369670799ae0a74d4d5a44266cb7
Hakim Cassimally <Hakim.Cassimally@gmail.com>
2026-09-11 13:01:05 +0100

    DOC-14537 attribute no longer used

--- gulp.d/tasks/build-preview-pages.js
+++ gulp.d/tasks/build-preview-pages.js
@@ -50,7 +50,6 @@ const ASCIIDOC_ATTRIBUTES = {
   'source-highlighter': 'highlight.js',
   'page-watermark': '2025-01-09 docs-sdk-nodejs add-preview-metadata',
   'page-show-preview-banner': true,
-  'page-chatbot-origin': 'https://d2tkqbbgxczr0v.cloudfront.net',
 }
 
 module.exports =
